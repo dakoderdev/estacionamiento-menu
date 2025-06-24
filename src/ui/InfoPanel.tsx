9310f153b81e212198d65e81d51bc68bbdc4778f
dakodev <sweetdawinner@gmail.com>
2025-06-24 18:29:10 -0300

    alert on unfinished buttons

--- src/ui/InfoPanel.tsx
+++ src/ui/InfoPanel.tsx
@@ -149,16 +149,13 @@ export default function InfoPanel({
             </button>
           </div>
           <div className="flex gap-1.5">
-            <button className="text-xs py-0.5 px-2 rounded-full bg-white/60 hover:bg-white/30 focus:bg-white/30 transition-colors cursor-pointer grow text-black justify-center items-center flex">
-              {/* ...SVG... */}
+            <button onClick={() => alert("Funcionalidad no implementada")} className="text-xs py-0.5 px-2 rounded-full bg-white/60 hover:bg-white/30 focus:bg-white/30 transition-colors cursor-pointer grow text-black justify-center items-center flex">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 5l0 2" /><path d="M15 11l0 2" /><path d="M15 17l0 2" /><path d="M5 5h14a2 2 0 0 1 2 2v3a2 2 0 0 0 0 4v3a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-3a2 2 0 0 0 0 -4v-3a2 2 0 0 1 2 -2" /></svg>
             </button>
-            <button className="text-xs py-0.5 px-2 rounded-full bg-white/60 hover:bg-white/30 focus:bg-white/30 transition-colors cursor-pointer grow text-black justify-center items-center flex">
-              {/* ...SVG... */}
+            <button onClick={() => alert("Funcionalidad no implementada")} className="text-xs py-0.5 px-2 rounded-full bg-white/60 hover:bg-white/30 focus:bg-white/30 transition-colors cursor-pointer grow text-black justify-center items-center flex">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M10 20.777a8.942 8.942 0 0 1 -2.48 -.969" /><path d="M14 3.223a9.003 9.003 0 0 1 0 17.554" /><path d="M4.579 17.093a8.961 8.961 0 0 1 -1.227 -2.592" /><path d="M3.124 10.5c.16 -.95 .468 -1.85 .9 -2.675l.169 -.305" /><path d="M6.907 4.579a8.954 8.954 0 0 1 3.093 -1.356" /><path d="M12 8v4l3 3" /></svg>
             </button>
-            <button className="text-xs py-0.5 px-2 rounded-full bg-white/60 hover:bg-white/30 focus:bg-white/30 transition-colors cursor-pointer grow text-black justify-center items-center flex">
-              {/* ...SVG... */}
+            <button onClick={() => alert("Funcionalidad no implementada")} className="text-xs py-0.5 px-2 rounded-full bg-white/60 hover:bg-white/30 focus:bg-white/30 transition-colors cursor-pointer grow text-black justify-center items-center flex">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" /><path d="M13.5 6.5l4 4" /></svg>
             </button>
           </div>
